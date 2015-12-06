46094571eee3e2dfcd2b9a9c6de61731d988cac2
Emily Stamey <elstamey@ncsu.edu>
2015-12-06 16:23:09 -0500

    completed day 2 part one

--- src/application.php
+++ src/application.php
@@ -6,10 +6,11 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Acme\Console\Command\AdventPuzzleCommand;
 use Acme\Console\Command\GreetCommand;
-use Acme\Console\Command\CfpCommand;
+use Acme\Console\Command\DayTwoCommand;
 use Symfony\Component\Console\Application;
 
 $application = new Application();
 $application->add(new GreetCommand());
 $application->add(new AdventPuzzleCommand());
+$application->add(new DayTwoCommand());
 $application->run();
