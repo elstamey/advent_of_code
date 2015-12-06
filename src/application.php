0d0d6f0424b60ffbed47ba48b284b51fa61e117f
Emily Stamey <elstamey@ncsu.edu>
2015-12-06 13:55:20 -0500

    first pass at day 1 puzzle

---
+++ src/application.php
@@ -0,0 +1,15 @@
+#!/usr/bin/env php
+<?php
+// application.php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Acme\Console\Command\AdventPuzzleCommand;
+use Acme\Console\Command\GreetCommand;
+use Acme\Console\Command\CfpCommand;
+use Symfony\Component\Console\Application;
+
+$application = new Application();
+$application->add(new GreetCommand());
+$application->add(new AdventPuzzleCommand());
+$application->run();
