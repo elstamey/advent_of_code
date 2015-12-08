8dbcdf457d1ca36a40f6bf391bd2fc49fff5a77b
Emily Stamey <elstamey@ncsu.edu>
2015-12-07 20:44:15 -0500

    completed day 4 parts 1 & 2

--- src/application.php
+++ src/application.php
@@ -8,6 +8,7 @@ use Acme\Console\Command\AdventPuzzleCommand;
 use Acme\Console\Command\GreetCommand;
 use Acme\Console\Command\DayTwoCommand;
 use Acme\Console\Command\DayThreeCommand;
+use Acme\Console\Command\DayFourCommand;
 use Symfony\Component\Console\Application;
 
 $application = new Application();
@@ -15,4 +16,5 @@ $application->add(new GreetCommand());
 $application->add(new AdventPuzzleCommand());
 $application->add(new DayTwoCommand());
 $application->add(new DayThreeCommand());
+$application->add(new DayFourCommand());
 $application->run();
