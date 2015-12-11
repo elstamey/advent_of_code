c450a20cd774d817be94d072b6326a19289f700c
Emily Stamey <elstamey@ncsu.edu>
2015-12-10 21:50:23 -0500

    completed part 2 but added way too many extra lines of code

--- src/Console/Command/DaySixCommand.php
+++ src/Console/Command/DaySixCommand.php
@@ -46,13 +46,23 @@ class DaySixCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->input_string = file_get_contents($input->getArgument('inputFile'));
-        $this->map = array_fill(0, 999, array_fill(0, 999, 'off'));
+        $this->map = array_fill(0, 999, array_fill(0, 999, 0));
 
         if ($input->getOption('part2')) {
             foreach (preg_split("/\n/", $this->input_string) as $line) {
                 if (isset($line) && ($line != "")) {
+                    $instructions = preg_split("/ /", $line);
+                    if ($instructions[0] == "toggle") {
+                        $this->toggle2($instructions);
+                    } elseif ($instructions[0] == "turn") {
+                        $this->turn2($instructions);
+                    } else {
+                        print($line."\n");
+                    }
                 }
             }
+            $result = $this->returnTotalBrightness($this->map);
+
         } else {
             foreach (preg_split("/\n/", $this->input_string) as $line) {
                 if (isset($line) && ($line != "")) {
@@ -66,10 +76,10 @@ class DaySixCommand extends Command
                     }
                 }
             }
+            $result = $this->returnNumberOfOnLights($this->map);
         }
-        $result = $this->returnOnLights($this->map);
 
-        $output->writeln("result = " . count($result));
+        $output->writeln("result = " . $result);
     }
 
     private function toggle( $instructions )
@@ -80,6 +90,14 @@ class DaySixCommand extends Command
         $this->buildMap($from, $to);
     }
 
+    private function toggle2( $instructions )
+    {
+        $from = preg_split("/,/", $instructions[1]);
+        $to = preg_split("/,/", $instructions[3]);
+
+        $this->buildMap2($from, $to, 2);
+    }
+
     private function turn( $instructions )
     {
         $from = preg_split("/,/", $instructions[2]);
@@ -88,16 +106,29 @@ class DaySixCommand extends Command
         $this->buildMap($from, $to, $instructions[1]);
     }
 
+    private function turn2( $instructions )
+    {
+        $from = preg_split("/,/", $instructions[2]);
+        $to = preg_split("/,/", $instructions[4]);
+
+        if ($instructions[1] == 'on') {
+            $val = 1;
+        } elseif ($instructions[1] == 'off') {
+            $val = -1;
+        }
+        $this->buildMap2($from, $to, $val);
+    }
+
     private function buildMap($from, $to, $val=null)
     {
         for ($i=$from[0]; $i <= $to[0]; $i++) {
             for ($j=$from[1]; $j <= $to[1]; $j++) {
                 if (isset($val)) {
                     $this->map[$i][$j] = $val;
-                } elseif ((!isset($this->map[$i][$j])) or ($this->map[$i][$j] == "off")) {
-                    $this->map[$i][$j] = "on";
-                } elseif ($this->map[$i][$j] == "on") {
-                    $this->map[$i][$j] = "off";
+                } elseif ((!isset($this->map[$i][$j])) or ($this->map[$i][$j] == 0)) {
+                    $this->map[$i][$j] = 1;
+                } elseif ($this->map[$i][$j] == 1) {
+                    $this->map[$i][$j] = 0;
                 } else {
                     throw new InvalidArgumentException('invalid coordinate');
                 }
@@ -105,16 +136,44 @@ class DaySixCommand extends Command
         }
     }
 
-    private function returnOnLights($map)
+    private function buildMap2($from, $to, $val)
+    {
+        for ($i=$from[0]; $i <= $to[0]; $i++) {
+            for ($j=$from[1]; $j <= $to[1]; $j++) {
+                if (isset($this->map[$i][$j])) {
+                    $this->map[$i][$j] += $val;
+                } else {
+                    $this->map[$i][$j] = $val;
+                }
+
+                $this->map[$i][$j] = ($this->map[$i][$j] < 0 ) ? 0 : $this->map[$i][$j] ;
+            }
+        }
+    }
+
+    private function returnNumberOfOnLights($map)
     {
         $newMap = [];
         for ($i=0; $i <= 999; $i++) {
             for ($j=0; $j <= 999; $j++) {
-                if (isset($this->map[$i][$j]) && $this->map[$i][$j] == "on") {
-                    array_push($newMap, "on");
+                if (isset($map[$i][$j]) && $map[$i][$j] == 1) {
+                    array_push($newMap, 1);
                 }
             }
         }
-        return $newMap;
+        return count($newMap);
+    }
+
+    private function returnTotalBrightness($map)
+    {
+        $brightness = 0;
+        for ($i=0; $i <= 999; $i++) {
+            for ($j=0; $j <= 999; $j++) {
+                if (isset($map[$i][$j]) && ($map[$i][$j] >= 0)) {
+                   $brightness += $map[$i][$j];
+                }
+            }
+        }
+        return $brightness;
     }
 }
