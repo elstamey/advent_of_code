d6eafdcae6d496e83c040f380923e9a143e451c7
Emily Stamey <elstamey@ncsu.edu>
2015-12-13 23:35:40 -0500

    partial attempt

--- src/Console/Command/DayNineCommand.php
+++ src/Console/Command/DayNineCommand.php
@@ -21,6 +21,10 @@ class DayNineCommand extends Command
      */
     var $input_string = 'bgvyzdsv';
 
+    var $locations = [];
+
+    var $distances = [];
+
     protected function configure()
     {
         $this
@@ -47,9 +51,32 @@ class DayNineCommand extends Command
         } else {
             foreach (preg_split( "/\n/", $this->input_string ) as $line) {
                 if (isset( $line ) && ( $line != "" )) {
+                    $lArray = preg_split("/[\ \=]+/", $line);
+                    print($lArray[0]." ".$lArray[2]." ".$lArray[3]."\n");
+                    $loc1 = $lArray[0];
+                    $loc2 = $lArray[2];
+                    $dist = $lArray[3];
+                    $this->setLocations($loc1, $loc2);
+                    $this->saveDistance($loc1, $loc2, $dist);
                 }
             }
         }
-        $output->writeln( "result = " . $result );
+//        $output->writeln( "result = " . $result );
+    }
+
+    private function setLocations( $loc1, $loc2 )
+    {
+        if (!in_array($loc1, $this->locations)) {
+            array_push($this->locations, $loc1);
+        }
+        if (!in_array($loc2, $this->locations)) {
+            array_push($this->locations, $loc2);
+        }
+    }
+
+    private function saveDistance( $loc1, $loc2, $dist )
+    {
+        $this->distances[$loc1][$loc2] = $dist;
+        $this->distances[$loc2][$loc1] = $dist;
     }
 }
