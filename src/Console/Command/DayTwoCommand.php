63b1ece2502b062918dc5752723ada6eef2590c2
Emily Stamey <elstamey@ncsu.edu>
2016-12-03 17:31:11 -0500

    commented print statements; fixed the pointer movement

--- src/Console/Command/DayTwoCommand.php
+++ src/Console/Command/DayTwoCommand.php
@@ -43,7 +43,7 @@ class DayTwoCommand extends Command
             array_push($keys, $this->decodeKeyPress($line));
         }
 
-        print("keys: " . implode('', $keys) ."\n");
+//        print("keys: " . implode('', $keys) ."\n");
 
         return implode('', $keys);
     }
@@ -62,35 +62,39 @@ class DayTwoCommand extends Command
 
                 switch ($m) {
                     case 'U':
-                        $y--;
-                        if ($y < 0) {
-                            $y = 0;
-                        }
-                        break;
-                    case 'D':
-                        $y++;
-                        if ($y > 2) {
-                            $y = 2;
-                        }
-                        break;
-                    case 'L':
                         $x--;
                         if ($x < 0) {
                             $x = 0;
                         }
                         break;
-                    case 'R':
+                    case 'D':
                         $x++;
                         if ($x > 2) {
                             $x = 2;
                         }
                         break;
+                    case 'L':
+                        $y--;
+                        if ($y < 0) {
+                            $y = 0;
+                        }
+                        break;
+                    case 'R':
+                        $y++;
+                        if ($y > 2) {
+                            $y = 2;
+                        }
+                        break;
                 }
+//                print "(".$x. "," . $y . ") = ". $this->keypad[$x][$y];
+                $this->position = [$x, $y];
             }
+
+
+//        print "key: " . $this->keypad[$x][$y] . "\n";
+            return $this->keypad[$x][$y];
         }
 
-        print "key: " . $this->keypad[$x][$y] . "\n";
-        return $this->keypad[$x][$y];
     }
 
 }
