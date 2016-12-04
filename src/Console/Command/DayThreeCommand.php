143bedf12a48d28708fe8055ecff440ec40448b5
Emily Stamey <elstamey@ncsu.edu>
2016-12-04 00:37:09 -0500

    added part2 tests and code; success

--- src/Console/Command/DayThreeCommand.php
+++ src/Console/Command/DayThreeCommand.php
@@ -4,6 +4,7 @@ namespace Acme\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class DayThreeCommand extends Command
@@ -15,22 +16,51 @@ class DayThreeCommand extends Command
         $this
             ->setName('day3')
             ->setDescription('Day 3: Squares With Three Sides')
-            ->addArgument('inputFile', null, 'newFile', 'day3.txt');
+            ->addArgument('inputFile', null, 'newFile', 'day3.txt')
+            ->addOption(
+                'part2',
+                null,
+                InputOption::VALUE_NONE,
+                'If set, the part two puzzle will be solved'
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->input_string = file_get_contents($input->getArgument('inputFile'));
 
-        foreach (preg_split("/\n/", $this->input_string) as $line) {
-            if (isset($line) && ($line != "")) {
-                $coords = preg_split("/[\s]+/", $line);
+        if ($input->getOption('part2')) {
+
+            $this->validTriangles = 0;
+
+            $groups = array_chunk(preg_split("/[\n]/", $this->input_string), 3);
+            foreach ($groups as $group) {
+
+                if (count($group) == 3) {
+                    $g = $group[0] . $group[1] . $group[2];
+
+                    if (isset($g) && ($g != "")) {
+                        $x = preg_split("/[\s]+/", $g);
+
+                        $this->testValidTriangle($x[1], $x[4], $x[7]);
+                        $this->testValidTriangle($x[2], $x[5], $x[8]);
+                        $this->testValidTriangle($x[3], $x[6], $x[9]);
+                    }
+                }
+            }
+
+        } else {
+            foreach (preg_split("/\n/", $this->input_string) as $line) {
+                if (isset($line) && ($line != "")) {
+                    $coords = preg_split("/[\s]+/", $line);
 
 //                var_dump($coords);
 //                print "\n";
 
-                $this->testValidTriangle($coords[1], $coords[2], $coords[3]);
+                    $this->testValidTriangle($coords[1], $coords[2], $coords[3]);
+                }
             }
+
         }
 
         $result = $this->validTriangles;
--- tests/DayThreeCommandTest.php
+++ tests/DayThreeCommandTest.php
@@ -32,6 +32,31 @@ class DayThreeCommandTest extends \PHPUnit_Framework_TestCase
     }
 
     /** @test */
+    public function testExecutePartTwo()
+    {
+        $application = new Application();
+        $application->add(new DayThreeCommand());
+
+        $command = $application->find('day3');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array(
+            'command'  => $command->getName(),
+
+            // pass arguments to the helper
+            'inputFile' => 'testday3.txt',
+
+            '--part2' => true,
+
+            // prefix the key with a double slash when passing options,
+            // e.g: '--some-option' => 'option_value',
+        ));
+
+
+        // the output of the command in the console
+        $output = $commandTester->getDisplay();
+        $this->assertContains('result = 6', $output);
+    }
+    /** @test */
     function testIsTriangle()
     {
         $this->assertTrue(DayThreeCommand::isTriangle(1, 2, 2), 'Did not get valid triangle, expecting valid');
