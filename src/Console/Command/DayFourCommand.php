981c99512ebeaa240b5108b6b6b099077932c231
Emily Stamey <elstamey@ncsu.edu>
2016-12-04 21:49:55 -0500

    initial pass, not working. Having trouble with the sorting

---
+++ src/Console/Command/DayFourCommand.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Acme\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DayFourCommand extends Command
+{
+
+    /**
+     * @var int
+     */
+    private $sumOfSectorIds = 0;
+
+
+    protected function configure()
+    {
+        $this
+            ->setName('day4')
+            ->setDescription('Day 4: Security Through Obscurity')
+            ->addArgument('inputFile', null, 'newFile', 'day4.txt')
+            ->addOption(
+                'part2',
+                null,
+                InputOption::VALUE_NONE,
+                'If set, the part two puzzle will be solved'
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->input_string = file_get_contents($input->getArgument('inputFile'));
+
+        if ($input->getOption('part2')) {
+            foreach (preg_split("/\n/", $this->input_string) as $line) {
+                if (isset($line) && ($line != "")) {
+                }
+            }
+        } else {
+            foreach (preg_split("/\n/", $this->input_string) as $line) {
+                if (isset($line) && ($line != "")) {
+                    $this->isRealRoom($line);
+
+                    print "Sector Sum: " . $this->getSectorSum() . "\n";
+                }
+            }
+        }
+        $result = $this->getSectorSum();
+        $output->writeln("result = " . $result);
+    }
+
+    /**
+     * @param string $roomInfo
+     *
+     * @return bool
+     */
+    protected function isRealRoom($roomInfo)
+    {
+        preg_match('#([\w\-]+)([\d]{3})\[([\w]+)\]#', $roomInfo, $matches);
+
+        $roomName = preg_replace("/-/", "", $matches[1]);
+        $sectorId = $matches[2];
+        $checksum = $matches[3];
+
+        $roomNameParts = str_split($roomName, 1);
+        $reduce = array_count_values($roomNameParts);
+        asort($reduce);
+
+        var_dump($reduce);
+
+        $reduce = array_slice($reduce, 0, 5);
+        $roomNameParts = array_keys($reduce);
+        natsort($roomNameParts);
+
+
+        $checkSumArray = str_split($checksum, 1);
+
+//        var_dump($roomNameParts, $checkSumArray);
+//        print "\n";
+
+        if ( count(array_intersect($roomNameParts, $checkSumArray)) === 5) {
+            print "sector id: " . $sectorId . "\n";
+            $this->sumOfSectorIds += $sectorId;
+            return true;
+        } else {
+            print "NOT REAL!!!! ------\n";
+            var_dump($reduce, $roomNameParts, $checkSumArray);
+            print "\n";
+            return false;
+        }
+
+    }
+
+    /**
+     * @return int
+     */
+    public function getSectorSum()
+    {
+        return $this->sumOfSectorIds;
+    }
+
+
+
+}
---
+++ tests/DayFourCommandTest.php
@@ -0,0 +1,71 @@
+<?php
+
+use Acme\Console\Command\DayFourCommand;
+use Symfony\Component\Console\Application;
+use \Symfony\Component\Console\Tester\CommandTester;
+
+class DayFourCommandTest extends \PHPUnit_Framework_TestCase
+{
+
+    /** @test */
+    public function testExecute()
+    {
+        $application = new Application();
+        $application->add(new DayFourCommand());
+
+        $command = $application->find('day4');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array(
+            'command'  => $command->getName(),
+
+            // pass arguments to the helper
+            'inputFile' => 'testday4.txt',
+
+            // prefix the key with a double slash when passing options,
+            // e.g: '--some-option' => 'option_value',
+        ));
+
+
+        // the output of the command in the console
+        $output = $commandTester->getDisplay();
+        $this->assertContains('result = 1514', $output);
+    }
+//
+//    /** @test */
+//    public function testExecutePartTwo()
+//    {
+//        $application = new Application();
+//        $application->add(new DayFourCommand());
+//
+//        $command = $application->find('day4');
+//        $commandTester = new CommandTester($command);
+//        $commandTester->execute(array(
+//            'command'  => $command->getName(),
+//
+//            // pass arguments to the helper
+//            'inputFile' => 'testday4.txt',
+//
+//            // prefix the key with a double slash when passing options,
+//            // e.g: '--some-option' => 'option_value',
+//            '--part2' => true,
+//
+//        ));
+//
+//
+//        // the output of the command in the console
+//        $output = $commandTester->getDisplay();
+//        $this->assertContains('result = 6', $output);
+//    }
+
+//    /** @test */
+//    function testIsRealRoom()
+//    {
+//        $this->assertTrue(DayFourCommand::isRealRoom('aaaaa-bbb-z-y-x-123[abxyz]'), 'Did not return true, expecting real room');
+//        $this->assertTrue(DayFourCommand::isRealRoom('a-b-c-d-e-f-g-h-987[abcde]'), 'Did not return true, expecting real room');
+//        $this->assertTrue(DayFourCommand::isRealRoom('not-a-real-room-404[oarel]'), 'Did not return true, expecting real room');
+//        $this->assertFalse(DayFourCommand::isRealRoom('totally-real-room-200[decoy]'), 'Got real room, expecting not real');
+//
+////        $this->assertEquals(1514, DayFourCommand::getSectorSum(), 'Returned a Sum that did not match expectations');
+//    }
+
+}
