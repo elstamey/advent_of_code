2b49af6d512c0e177bc95af3d32148a4d682cce6
Emily Stamey <elstamey@ncsu.edu>
2016-12-05 19:50:52 -0500

    added part 2 code and got the correct answer

--- src/Console/Command/DayFiveCommand.php
+++ src/Console/Command/DayFiveCommand.php
@@ -20,6 +20,8 @@ class DayFiveCommand extends Command
 
     var $password = '';
 
+    var $passwordArray = [];
+
     protected function configure()
     {
         $this
@@ -36,10 +38,18 @@ class DayFiveCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         if ($input->getOption('part2')) {
-            foreach (preg_split("/\n/", $this->input_string) as $line) {
-                if (isset($line) && ($line != "")) {
-                }
+            while (count($this->passwordArray) < 8) {
+                $this->hashItPositionally($this->doorId, $this->currentIndex);
+
+                $this->currentIndex++;
             }
+
+            ksort($this->passwordArray, SORT_NUMERIC);
+
+            var_dump($this->passwordArray);
+
+            $this->password = implode('', $this->passwordArray);
+
         } else {
             while (strlen($this->password) < 8) {
                 $this->hashIt($this->doorId, $this->currentIndex);
@@ -62,6 +72,24 @@ class DayFiveCommand extends Command
             var_dump($passwordCharacter, $hashedString);
 
             $this->password = $this->password . $passwordCharacter;
+        }
+    }
+
+    public function hashItPositionally($door, $input)
+    {
+        $hashedString = md5($door.$input);
+
+        if (substr($hashedString, 0, 5) === '00000') {
+
+            $position = substr($hashedString, 5,1);
+            if (is_numeric($position) && (0 <= $position) && ($position  <= 7) && !isset($this->passwordArray[$position])) {
+                $position = intval($position);
+                $passwordCharacter = substr($hashedString, 6, 1);
+
+                var_dump($hashedString, $position, $passwordCharacter);
+
+                $this->passwordArray[$position] = $passwordCharacter;
+            }
         }
     }
 
